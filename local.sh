cfebdb32434ce1bab44aca0a15ff1683695db805
uraway <masato.u0107@gmail.com>
2016-04-08 22:27:50 +0900

    remove some scripts

---
+++ local.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export SLACK_TOKEN=xoxp-21725596643-32714130066-32756027265-b366becdec \
+export SLACK_CLIENT_ID=21725596643.32748832064 \
+export SLACK_CLIENT_SECRET=39294af508d62631068307f3018739ba \
+export HUBOT_SLACK_TOKEN=xoxb-32702035460-pxO8b6mBdcWDd6HRSsXo3UkF \
+export KUROMOJI_DIC_PATH="./dict" \
+export HUBOT_GOOGLE_OAUTH_CLIENT_ID="456659677711-6i7trhb9nlsmdon5ihr7vmig9l1fldi7.apps.googleusercontent.com" \
+export HUBOT_GOOGLE_OAUTH_CLIENT_SECRET="OMFIDjHJ_gmw4H6doUVRgS1E" \
+
+./bin/hubot --adapter slack
+
+HUBOT_GOOGLE_OAUTH_CLIENT_ID:     456659677711-6i7trhb9nlsmdon5ihr7vmig9l1fldi7.apps.googleusercontent.com
+HUBOT_GOOGLE_OAUTH_CLIENT_SECRET: OMFIDjHJ_gmw4H6doUVRgS1E
+HUBOT_GOOGLE_SITE_VERIFICATION:   https://stormy-waters-17617.herokuapp.com/
+HUBOT_HEROKU_KEEPALIVE_URL:       https://stormy-waters-17617.herokuapp.com/
+HUBOT_SLACK_BOTNAME:              nishida
+HUBOT_SLACK_TEAM:                 tnishida-lab
+HUBOT_SLACK_TOKEN:                xoxb-32702035460-pxO8b6mBdcWDd6HRSsXo3UkF
+HUBOT_SLACK_URL:                  https://stormy-waters-17617.herokuapp.com/
+KUROMOJI_DIC_PATH:                ./dict
+SLACK_CLIENT_ID:                  21725596643.32748832064
+SLACK_CLIENT_SECRET:              39294af508d62631068307f3018739ba
+SLACK_TOKEN:                      xoxp-21725596643-32714130066-32756027265-b366becdec
